fdc76ab04d50c6c95da51f6ec4a250309cdf225a
Kevin Martinez <157753600+KevinMartinez5058@users.noreply.github.com>
2024-06-09 02:36:40 -0500

    Delete 50 projects in 50 days/50 projects in 50 days directory

--- 50 projects in 50 days/50 projects in 50 days/Day 4/app.js
+++
@@ -1,8 +0,0 @@
-const search = document.querySelector('.search');
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-
-btn.addEventListener('click', () => {
-  search.classList.toggle('active'); // we seen what add and remove does now toggle is a mix of both. you click and it will give you an add if you click again it removes the class
-  input.focus(); //the focus method like it implies focuses on that element so in this case the input of the search bar will be focused and ready to type in the search bar
-});
